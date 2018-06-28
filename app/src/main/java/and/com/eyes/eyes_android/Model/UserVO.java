e6bae2a2ded2b87f1f39536b018dcc6bf567dd9a
MangKyu <whalsrb1226@naver.com>
2018-06-29 00:37:53 +0900

    add User Model Template

---
+++ app/src/main/java/and/com/eyes/eyes_android/Model/UserVO.java
@@ -0,0 +1,22 @@
+package and.com.eyes.eyes_android.Model;
+
+public class UserVO {
+    String userName;
+    String userAge;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+}
