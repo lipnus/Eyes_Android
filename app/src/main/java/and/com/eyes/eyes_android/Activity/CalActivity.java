3a09a5bc85090e973adc1c2ff65cdd8006283fdd
cocobino <coolbino@naver.com>
2018-06-29 15:23:28 +0900

    android : call Activity modify

---
+++ app/src/main/java/and/com/eyes/eyes_android/Activity/CalActivity.java
@@ -0,0 +1,18 @@
+package and.com.eyes.eyes_android.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import and.com.eyes.eyes_android.R;
+
+public class CalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal);
+
+
+
+    }
+}
